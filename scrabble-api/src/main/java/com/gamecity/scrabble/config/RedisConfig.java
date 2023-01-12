ce21ef5e27b73d9336ab5dc240d8b6f53ee553fe
Erdem Karakus <erdem.karakus@yahoo.com>
2023-01-12 12:24:49 +0100

    Improve Redis logging

--- scrabble-api/src/main/java/com/gamecity/scrabble/config/RedisConfig.java
+++ scrabble-api/src/main/java/com/gamecity/scrabble/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.gamecity.scrabble.config;
 
 import java.time.Duration;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -20,6 +21,8 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Spring configuration for Redis including push messages and cache connection
  * 
@@ -28,6 +31,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableCaching
 @PropertySource("classpath:redis.properties")
+@Slf4j
 public class RedisConfig extends CachingConfigurerSupport {
 
     private static final Integer TTL_SECONDS = 3600;
@@ -49,8 +53,9 @@ public class RedisConfig extends CachingConfigurerSupport {
 
     @Bean
     JedisConnectionFactory connectionFactory() {
+        log.debug("Connecting to '{}:{}'", redisHost, redisPort);
         final RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
-        if (redisUsername != null && redisPassword != null) {
+        if (StringUtils.isNotEmpty(redisUsername) && StringUtils.isNotEmpty(redisPassword)) {
             redisConfiguration.setUsername(redisUsername);
             redisConfiguration.setPassword(RedisPassword.of(redisPassword));
         }
@@ -59,6 +64,7 @@ public class RedisConfig extends CachingConfigurerSupport {
         jedisConfigurationBuilder.connectTimeout(Duration.ofSeconds(60));
         jedisConfigurationBuilder.readTimeout(Duration.ofSeconds(60));
         if (useSsl) {
+            log.debug("Redis server is ssl secured");
             jedisConfigurationBuilder.useSsl();
         }
         jedisConfigurationBuilder.usePooling();
--- scrabble-gateway/src/main/java/com/gamecity/scrabble/config/RedisConfig.java
+++ scrabble-gateway/src/main/java/com/gamecity/scrabble/config/RedisConfig.java
@@ -2,12 +2,15 @@ package com.gamecity.scrabble.config;
 
 import java.time.Duration;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
@@ -17,21 +20,25 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.gamecity.scrabble.controller.ActionController;
 import com.gamecity.scrabble.controller.ChatController;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Spring configuration for Redis
  * 
  * @author ekarakus
  */
-@SuppressWarnings("rawtypes")
 @Configuration
 @PropertySource("classpath:redis.properties")
+@Slf4j
 public class RedisConfig extends CachingConfigurerSupport {
 
+    private static final Integer TTL_SECONDS = 3600;
     private static final String ACTION = "ACTION";
     private static final String CHATS = "CHATS";
 
@@ -58,8 +65,9 @@ public class RedisConfig extends CachingConfigurerSupport {
 
     @Bean
     JedisConnectionFactory connectionFactory() {
+        log.debug("Connecting to '{}:{}'", redisHost, redisPort);
         final RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
-        if (redisUsername != null && redisPassword != null) {
+        if (StringUtils.isNotEmpty(redisUsername) && StringUtils.isNotEmpty(redisPassword)) {
             redisConfiguration.setUsername(redisUsername);
             redisConfiguration.setPassword(RedisPassword.of(redisPassword));
         }
@@ -68,6 +76,7 @@ public class RedisConfig extends CachingConfigurerSupport {
         jedisConfigurationBuilder.connectTimeout(Duration.ofSeconds(60));
         jedisConfigurationBuilder.readTimeout(Duration.ofSeconds(60));
         if (useSsl) {
+            log.debug("Redis server is ssl secured");
             jedisConfigurationBuilder.useSsl();
         }
         jedisConfigurationBuilder.usePooling();
@@ -78,12 +87,25 @@ public class RedisConfig extends CachingConfigurerSupport {
     }
 
     @Bean
-    RedisTemplate redisTemplate(JedisConnectionFactory connectionFactory) {
+    RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
         final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
+    }
+
+    @Bean
+    RedisCacheManager cacheManager(JedisConnectionFactory connectionFactory) {
+        RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
+                        .disableCachingNullValues()
+                        .computePrefixWith(name -> name + ":")
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair
+                                .fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                        .entryTtl(Duration.ofSeconds(TTL_SECONDS)))
+                .build();
+        return redisCacheManager;
     }
 
     @Bean
